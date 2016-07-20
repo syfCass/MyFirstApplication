296fb26d4a28b7ef93d66d8a0cefd238481f5841
syfCass <353015520@qq.com>
2016-07-20 10:55:57 +0800

    Rejected commit: Default

--- app/src/main/java/com/example/cass/myfirstapplication/NewClass.java
+++ app/src/main/java/com/example/cass/myfirstapplication/NewClass.java
@@ -5,6 +5,20 @@ package com.example.cass.myfirstapplication;
  */
 public class NewClass {
     private String name;
+    private String id;
+
+    public NewClass(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
